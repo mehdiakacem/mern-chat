852d34226bc6ad5b8d1336253a4a936dbba1c224
Mehdi Akacem <96568645+mehdiakacem@users.noreply.github.com>
2024-07-07 20:06:38 +0100

    added websockests

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const User = require("./models/User");
+const ws = require("ws");
 
 dotenv.config();
 mongoose
@@ -92,4 +93,28 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.listen(4040);
+const server = app.listen(4040);
+
+const wss = new ws.WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")[1];
+      if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+    }
+  }
+
+  console.log([...wss.clients].length);
+});
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 export default function Chat() {
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:4040");
+    setWs(ws);
+    ws.addEventListener("message", handleMessage);
+  }, []);
+
+  function handleMessage(e) {
+    console.log("new message", e);
+  }
+
   return (
     <div className="flex h-screen">
       <div className="w-1/3 bg-white">contacts</div>
