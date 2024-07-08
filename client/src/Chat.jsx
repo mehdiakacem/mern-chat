ed38b43732eb489aaf31101289af107ec708cea8
Mehdi Akacem <96568645+mehdiakacem@users.noreply.github.com>
2024-07-08 15:55:13 +0100

    added user avatars

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Avatar from "./Avatar";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
@@ -41,8 +42,9 @@ export default function Chat() {
           MernChat
         </div>
         {Object.keys(onlinePeople).map((userId) => (
-          <div className="border-b border-gray-100 py-2">
-            {onlinePeople[userId]}
+          <div className="items-center flex border-b border-gray-100 py-2 gap-2 cursor-pointer">
+            <Avatar username={onlinePeople[userId]} userId={userId} />
+            <span className="text-gray-800">{onlinePeople[userId]}</span>
           </div>
         ))}
       </div>
